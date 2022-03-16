a5f26e2d0344832660f8beef2e121c0a9226c765
Harry Lum <hlum1@binghamton.edu>
2022-03-15 23:15:41 -0400

    add answers

--- src/data/answers.tsx
+++ src/data/answers.tsx
@@ -1,6 +1,22 @@
 const answers = [
   
   'harry',
+  'uclub',
+  'maine',
+  'dates',
+  'hikes',
+  'watch',
+  'vayne',
+  'yuumi',
+  'puppy',
+  'vchat',
+  'movie',
+  'lunch',
+  'parks',
+  'licks',
+  'buggy',
+  'bites',
+  'punch',
 ]
 
 export default answers
